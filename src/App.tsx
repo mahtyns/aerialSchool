7de7616e14c5e9dd61fc30f3a716b6aff0889672
Martyna <m.smolarek19@gmail.com>
2023-03-20 23:31:08 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from '@/components/scenes/Navbar'
+
+function App() {
+
+  return (
+    <div className="app bg-gradient-cream">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'gray-20': '#ece8e6',
+        'gray-50': '#ded8d4',
+        'gray-100': '#cdc5c2',
+        'gray-500': '#312d2a',
+        'primary-100': '#e6cdb9',
+        'primary-300': '#c79f7f',
+        'primary-500': '#ab7a53',
+        'secondary-400': '#f5cdc2',
+        'secondary-500': '#d9a395',
+        'special-500': '#4c6e61',
+      },
+      backgroundImage: (theme) => (
+        {
+          "gradient-cream" : "linear-gradient(175deg, rgba(255,249,245,1) 0%, rgba(236,236,236,1) 51%)",
+          "mobile-home" : "url()"
+        }
+      ),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sant-serif"]
+      },
+      content: {
+          // Add content for before and after pseudoclasses 
+          textShadow: "url(./assets/textShadow.png)"
+      }
+    },
+    screens: {
+      //Set classes for responsive screens
+      xs: "480px",
+      sm: "768px",
+      md: "1060px"
+    }
+  },
+  plugins: [],
+}
