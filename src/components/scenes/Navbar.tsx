dff76bcfa55df08f3285398e80b95986ca8f9984
Martyna <m.smolarek19@gmail.com>
2023-03-21 19:25:58 +0100

    navbar

--- src/components/scenes/Navbar.tsx
+++ src/components/scenes/Navbar.tsx
@@ -10,10 +10,24 @@ const Navbar = (props: Props) => {
 
     return (
         <nav>
-            <div
-                className={`${flexBetween} fixed top-0 z-30 w-full py-6`}
-            >
-
+            <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+                <div className={`${flexBetween} mx-auto w-5/6`}>
+                    <div className={`${flexBetween} w-full gap-16`}>
+                        <img alt="Volare School" src={Logo} className="w-56" />
+                        <div className={`${flexBetween} w-full gap-16`}>
+                            <div className={`${flexBetween} gap-8 text-lg`}>
+                                <p>Home</p>
+                                <p>Why us</p>
+                                <p>Our Courses</p>
+                                <p>Contact us</p>
+                            </div>
+                            <div className={`${flexBetween} gap-4 text-lg`}>
+                                <p>Sign in</p>
+                                <button>Join us</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </nav>
     )
